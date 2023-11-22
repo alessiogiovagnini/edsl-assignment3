e3279a06a00d2fcda0d508bd52bcf1e3e16fde95
alessiogiovagnini <alessio.giovagnini@usi.ch>
2023-11-22 19:15:57 +0100

    maybe solved es 2.1 and 2.2

--- src/main/scala/ch/usi/si/msde/edsl/assignment_03/Exercise2.scala
+++ src/main/scala/ch/usi/si/msde/edsl/assignment_03/Exercise2.scala
@@ -17,6 +17,58 @@ trait RequestAssertionDSL extends AssertionExecutor:
 
   // Implement the DSL here.
 
+  val tests = TestHandler()
+
+  case class TestHandler():
+    inline def += (newTest: Test) = {
+      val assertionWithDescription: AssertionWithDescription = newTest.getAssertionWithDescription
+      namedAssertions = namedAssertions ++ List(assertionWithDescription)
+    }
+
+  def test(description: String): Test = {
+    Test(description = description)
+  }
+
+
+  case class Test(description: String, response: Option[Future[Response]] = None, rePredicate: Option[ResponsePredicate] = None):
+    inline def calling (response: Future[Response]): Test = {
+      Test(description = description, response = Some(response))
+    }
+
+    inline def respondsWith (predicate: ResponsePredicate): Test = {
+      Test(description = description, response = response, rePredicate = Some(predicate))
+    }
+
+    inline def and (inputPredicate: ResponsePredicate): Test = {
+      val newPredicate: ResponsePredicate = rePredicate.get && inputPredicate
+      Test(description = description, response = response, rePredicate = Some(newPredicate))
+    }
+
+    inline def or (inputPredicate: ResponsePredicate): Test = {
+      val newPredicate: ResponsePredicate = rePredicate.get || inputPredicate
+      Test(description = description, response = response, rePredicate = Some(newPredicate))
+    }
+
+    def getAssertionWithDescription: AssertionWithDescription = {
+      val fun = () => response.get
+      val assertion: ExecutableAssertion = RequestSucceedsWithResponsePredicateAssertion(f = fun, responsePredicate = rePredicate.get)
+      AssertionWithDescription(description = description, assertion = assertion)
+    }
+
+  sealed trait ResponsePredicateType
+
+  case object statusCode extends ResponsePredicateType:
+    def ==(statusCode: Int): ResponsePredicate =
+      ResponseHasStatusCodeEqualsToPredicate(statusCode)
+
+  case object contentType extends ResponsePredicateType:
+    def ==(contentType: String): ResponsePredicate =
+      ResponseHasContentTypeEqualsToPredicate(contentType)
+
+  case object entity extends ResponsePredicateType:
+    def ==(entity: JsonObject): ResponsePredicate = ResponseContainsJson(
+      entity
+    )
 
 end RequestAssertionDSL
 
@@ -29,23 +81,23 @@ object Exercise2_1 extends RequestAssertionDSL with App:
 
   // Exercise 2.1: Basic respond/fail assertions
 
-  // tests += test("a get on user 2 will respond with 200 OK") calling {
-  //   GET { https("reqres.in") / "api" / "user" / "3" }
-  // } respondsWith statusCode == 200 and contentType == "application/json"
+   tests += test("a get on user 2 will respond with 200 OK") calling {
+     GET { https("reqres.in") / "api" / "user" / "3" }
+   } respondsWith statusCode == 200 and contentType == "application/json"
 
-  // tests += test("a get on a non-existing user will respond with 404") calling {
-  //   GET { https("reqres.in") / "api" / "user" / "-3721" }
-  // } respondsWith statusCode == 404
+   tests += test("a get on a non-existing user will respond with 404") calling {
+     GET { https("reqres.in") / "api" / "user" / "-3721" }
+   } respondsWith statusCode == 404
 
-  // tests += test("a login without password will respond with a json body containing an error") calling {
-  //      POST { 
-  //     https("reqres.in") / "api" / "login"
-  //   } withEntity json(
-  //     "email" `:` "morpheus@nebuchadnezzar"
-  //   )
-  // } respondsWith contentType == "application/json" and statusCode == 400 and entity == json(
-  //   "error" `:` "Missing password"
-  // )
+   tests += test("a login without password will respond with a json body containing an error") calling {
+        POST {
+       https("reqres.in") / "api" / "login"
+     } withEntity json(
+       "email" `:` "morpheus@nebuchadnezzar"
+     )
+   } respondsWith contentType == "application/json" and statusCode == 400 and entity == json(
+     "error" `:` "Missing password"
+   )
  
   // do ** NOT ** remove this
   run()
@@ -58,13 +110,13 @@ object Exercise2_2 extends App with RequestAssertionDSL:
   import HttpRequestDSL._
   import model.AsyncContext._
  
-  // tests += test("a get on a user will respond with 200 or 404") calling {
-  //   GET { https("reqres.in") / "api" / "user" / "3" }
-  // } respondsWith statusCode == 200 or statusCode == 404
+   tests += test("a get on a user will respond with 200 or 404") calling {
+     GET { https("reqres.in") / "api" / "user" / "3" }
+   } respondsWith statusCode == 200 or statusCode == 404
 
-  // tests += test("a get on a user will respond with 404 or 200 and contentType application/json") calling {
-  //   GET { https("reqres.in") / "api" / "user" / "3" }
-  // } respondsWith statusCode == 404 or statusCode == 200 and contentType == "application/json"
+   tests += test("a get on a user will respond with 404 or 200 and contentType application/json") calling {
+     GET { https("reqres.in") / "api" / "user" / "3" }
+   } respondsWith statusCode == 404 or statusCode == 200 and contentType == "application/json"
 
   // do ** NOT ** remove this
   run()
